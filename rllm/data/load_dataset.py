8f3ee2d4ead23071f9bead90f159b72715d9e1a4
Yury Orlovskiy <yuryo@berkeley.edu>
2025-01-11 12:53:46 +0000

    Adjust data

--- rllm/data/load_dataset.py
+++ rllm/data/load_dataset.py
@@ -1,5 +1,6 @@
 import enum
 import json
+import os
 from typing import Any, Dict, List, Union
 
 class Datasets(enum.Enum):
@@ -7,26 +8,38 @@ class Datasets(enum.Enum):
     OMNI = 'OMNI'
     NUMINA = 'NUMINA'
 
-def load_aime(path: str = "./raw/aime_v1.json") -> Union[List[Any], Dict[str, Any]]:
+class DatasetType(enum.Enum):
+    TRAIN = 'TRAIN'
+    TEST = 'TEST'
+
+def load_aime(dataset_type = DatasetType.TRAIN) -> Union[List[Any], Dict[str, Any]]:
     """
     Loads the AIME dataset from the given JSON file and returns its contents.
+
+    Convert AIME dataset into universal format ['problem', 'solution', 'answer', 'difficulty']
 
     :param path: Path to the AIME JSON file.
     :return: The loaded AIME data (usually a list or dict, depending on the file).
     """
-    with open(path, "r", encoding="utf-8") as f:
+    subfolder_name = 'train'
+    if dataset_type == DatasetType.TEST:
+        subfolder_name = 'test'
+    # Get the current directory path of this file
+    full_path = os.path.dirname(os.path.abspath(__file__)) + f'/raw/{subfolder_name}/aime.json'
+    with open(full_path, "r", encoding="utf-8") as f:
         data = json.load(f)
     return data
 
 
-def load_omni(path: str = "./raw/omni_math.json") -> Union[List[Any], Dict[str, Any]]:
+def load_omni() -> Union[List[Any], Dict[str, Any]]:
     """
     Loads the OMNI (OMNIMath) dataset from the given JSON file and returns its contents.
 
     :param path: Path to the OMNI JSON file.
     :return: The loaded OMNI data (usually a list or dict, depending on the file).
     """
-    with open(path, "r", encoding="utf-8") as f:
+    full_path = os.path.dirname(os.path.abspath(__file__)) + f'/raw/train/omni_math.jsonl'
+    with open(full_path, "r", encoding="utf-8") as f:
         data = json.load(f)
     return data
 
@@ -40,4 +53,3 @@ def load_dataset(dataset: Datasets):
         pass
         #return load_numina()
     raise ValueError('Invalid dataset')
-
