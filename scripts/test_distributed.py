3d2e6f410446d23e1852737f2f2e718557662fbc
jeffreysijuntan <sijuntan@yeah.net>
2025-01-12 11:21:47 +0000

    distributed vllm client

--- scripts/test_distributed.py
+++ scripts/test_distributed.py
@@ -1,25 +1,44 @@
-from vllm import EngineArgs
-from vllm.engine.arg_utils import AsyncEngineArgs
 
-from rllm.rollout.distributed import DistributedVLLM
-from rllm.utils import save_jsonl
+from rllm.data.load_dataset import Datasets, load_dataset
+from rllm.rollout.distributed_client import DistributedLLMClient
+from rllm.system_prompts import COT_MATH_SYSTEM_PROMPT
 
-from vllm import SamplingParams
+
+async def main():
+    dataset = load_dataset(Datasets.AIME)[:5]
+    messages_batch = [
+        [
+            {"role": "system", "content": COT_MATH_SYSTEM_PROMPT},
+            {"role": "user", "content": entry["problem"]},
+        ]
+        for entry in dataset
+    ]
+
+    endpoints = [
+        {"url": "http://0.0.0.0:8001/v1/chat/completions", "weight": 1.0},
+    ]
+    
+    
+    # Example sampling parameters
+    sampling_params = {
+        "temperature": 0.7,
+        "model":"Qwen/QwQ-32B-Preview",
+    }
+    
+    try:
+        client = await DistributedLLMClient.create_client(endpoints)
+        results = await client.chat_batch(messages_batch, sampling_params)
+        
+        # Print results
+        for i, result in enumerate(results):
+            print(f"Result {i}: {result}")
+            
+    except Exception as e:
+        print(f"An error occurred: {e}")
+    finally:
+        if 'client' in locals():
+            await client.__aexit__(None, None, None)
 
 if __name__ == "__main__":
-    model_name = "meta-llama/LLaMA-3.1.-8b-Instruct"
-
-    engine_args = AsyncEngineArgs(
-            model=model_name,
-            device="cuda",
-            tensor_parallel_size=2,
-    )
-
-    engine = DistributedVLLM(
-        num_workers=1,
-        engine_args=engine_args
-    )
-
-    engine.chat("hello", SamplingParams(), "id")
-
-    engine.shutdown()
+    import asyncio
+    asyncio.run(main())
