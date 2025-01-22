grpo fix kl penalty

verl/verl/trainer/main_reinforce.py
@@ -56,6 +56,11 @@ class RewardManager():
             compute_score_fn = self._select_rm_score_fn(data_source)
 
             score = compute_score_fn(solution_str=responses_str, ground_truth=ground_truth)
+
+            if i == 0:
+                print(responses_str)
+                print("ground truth:", ground_truth)
+
             rewards.append(score)
 
         return torch.tensor(rewards, dtype=torch.float)
@@ -132,7 +137,7 @@ def main_task(config):
     role_worker_mapping = {
         Role.ActorRollout: ray.remote(ActorRolloutRefWorker),
         # Role.Critic: ray.remote(CriticWorker),
-        Role.RefPolicy: ray.remote(ActorRolloutRefWorker)
+        # Role.RefPolicy: ray.remote(ActorRolloutRefWorker)
     }
 
     global_pool_id = 'global_pool'
@@ -142,7 +147,7 @@ def main_task(config):
     mapping = {
         Role.ActorRollout: global_pool_id,
         # Role.Critic: global_pool_id,
-        Role.RefPolicy: global_pool_id,
+        # Role.RefPolicy: global_pool_id,
     }
 
     # we should adopt a multi-source reward function here

verl/verl/trainer/reinforce/ray_trainer.py
@@ -79,37 +79,6 @@ import torch
 from verl.utils.torch_functional import masked_mean
 
 
-def apply_kl_penalty(data: DataProto, kl_ctrl: core_algos.AdaptiveKLController, kl_penalty='kl'):
-    responses = data.batch['responses']
-    response_length = responses.size(1)
-    token_level_scores = data.batch['token_level_scores']
-    batch_size = data.batch.batch_size[0]
-    attention_mask = data.batch['attention_mask']
-    response_mask = attention_mask[:, -response_length:]
-
-    # compute kl between ref_policy and current policy
-    if 'ref_log_prob' in data.batch.keys():
-        kld = core_algos.kl_penalty(data.batch['old_log_probs'], data.batch['ref_log_prob'],
-                                    kl_penalty=kl_penalty)  # (batch_size, response_length)
-        kld = kld * response_mask
-        beta = kl_ctrl.value
-    else:
-        beta = 0
-        kld = torch.zeros_like(response_mask, dtype=torch.float32)
-
-    token_level_rewards = token_level_scores - beta * kld
-
-    current_kl = masked_mean(kld, mask=response_mask, axis=-1)  # average over sequence
-    current_kl = torch.mean(current_kl, dim=0).item()
-
-    # according to https://github.com/huggingface/trl/blob/951ca1841f29114b969b57b26c7d3e80a39f75a0/trl/trainer/ppo_trainer.py#L837
-    kl_ctrl.update(current_kl=current_kl, n_steps=batch_size)
-    data.batch['token_level_rewards'] = token_level_rewards
-
-    metrics = {'critic/kl': current_kl, 'critic/kl_coeff': beta}
-
-    return data, metrics
-
 def reduce_metrics(metrics: dict):
     for key, val in metrics.items():
         metrics[key] = np.mean(val)
@@ -134,7 +103,6 @@ def _compute_response_info(batch):
 
 def compute_data_metrics(batch):
     # TODO: add response length
-    sequence_score = batch.batch['token_level_scores']
     sequence_reward = batch.batch['rewards']
 
     advantages = batch.batch['advantages']
@@ -145,10 +113,6 @@ def compute_data_metrics(batch):
     response_length = response_info['response_length']
 
     metrics = {
-        # score
-        'actor/scores/mean': masked_mean(sequence_score, response_mask).detach().item(),
-        'actor/scores/max': torch.max(sequence_score).detach().item(),
-        'actor/scores/min': torch.min(sequence_score).detach().item(),
         # reward
         'actor/rewards/mean': torch.mean(sequence_reward).detach().item(),
         'actor/rewards/max': torch.max(sequence_reward).detach().item(),
@@ -482,18 +446,12 @@ class RayReinforceTrainer(object):
 
                     # we combine with rule-based rm
                     rewards = self.reward_fn(batch)
-                    per_token_rewards = self.reward_fn.get_per_token_rewards(batch, rewards)
-                    batch.batch['token_level_scores'] = per_token_rewards
                     batch.batch['rewards'] = rewards
-
-                    # compute rewards. apply_kl_penalty if available
-                    batch, kl_metrics = apply_kl_penalty(batch,
-                                                         kl_ctrl=self.kl_ctrl,
-                                                         kl_penalty=self.config.algorithm.kl_penalty)
-                    metrics.update(kl_metrics)
 
                     # compute advantages, executed on the driver process
                     batch = self._compute_advantage(batch)
+                    # batch, kl_metrics = self._compute_kl(batch, kl_ctrl=self.kl_ctrl, kl_penalty=self.config.algorithm.kl_penalty)
+                    
 
                 # update critic
                 if self.use_critic:
@@ -580,3 +538,11 @@ class RayReinforceTrainer(object):
             raise NotImplementedError
 
         return data
+    
+
+    def _compute_kl(self, data: DataProto, kl_ctrl: core_algos.AdaptiveKLController, kl_penalty='kl'):
+        logprobs, ref_logprobs = data.batch["old_log_probs"], data.batch["ref_log_prob"]
+        log_ratio = ref_logprobs - logprobs
+        ratio = torch.exp(log_ratio)
+        approx_kl = (ratio - log_ratio - 1).mean()
+        return approx_kl

verl/verl/workers/actor/dp_actor.py
@@ -21,7 +21,7 @@ from torch import nn
 from torch.distributed.fsdp import FullyShardedDataParallel as FSDP
 
 from verl import DataProto
-from verl.trainer.ppo import core_algos
+from verl.trainer import core_algos
 from verl.workers.actor import BasePPOActor
 from verl.utils.py_functional import append_to_dict
 from verl.utils.torch_functional import logprobs_from_logits, log_probs_from_logits_all_rmpad
@@ -91,6 +91,12 @@ class DataParallelPPOActor(BasePPOActor):
         See PPO paper for details. https://arxiv.org/abs/1707.06347
         """
         select_keys = ['responses', 'input_ids', 'attention_mask', 'position_ids', 'old_log_probs', 'advantages']
+
+        if 'ref_log_probs' in data:
+            select_keys.append('ref_log_probs')
+
+        print(f"Size of the data: {len(data)}")
+
         data = data.select(batch_keys=select_keys)
         return data.make_iterator(mini_batch_size=self.config.ppo_mini_batch_size,
                                   epochs=self.config.ppo_epochs,
@@ -153,6 +159,7 @@ class DataParallelPPOActor(BasePPOActor):
 
         metrics = {}
         for batch_idx, data in enumerate(dataloader):
+            print("batch idx", batch_idx)
             # split batch into micro_batches
             micro_batches = data.batch.split(self.config.ppo_micro_batch_size)
 
@@ -187,14 +194,22 @@ class DataParallelPPOActor(BasePPOActor):
                     entropy_loss = core_algos.compute_entropy_loss(logits, full_response_mask_rmpad)  # (total_nnz,)
                 else:
                     entropy_loss = core_algos.compute_entropy_loss(logits, response_mask)
+                
+                # kl_coeff = 0.001 # ToDO: fix this
+                # kl_coeff = self.config.kl_coeff
+                # kl_loss = core_algos.compute_kl_loss(logprob=log_prob, ref_logprob=data['ref_log_prob'], eos_mask=response_mask)
+                
                 # compute policy loss
                 policy_loss = pg_loss - entropy_loss * entropy_coeff
+                # policy_loss = policy_loss - kl_loss * kl_coeff
+
 
                 loss = policy_loss / self.gradient_accumulation
                 loss.backward()
 
                 data = {
                     'actor/entropy_loss': entropy_loss.detach().item(),
+                    # 'actor/kl_loss': kl_loss.detach().item(),
                     'actor/pg_loss': pg_loss.detach().item(),
                     'actor/pg_clipfrac': pg_clipfrac.detach().item(),
                     'actor/ppo_kl': ppo_kl.detach().item(),