cc9a6c209f9cdfecafcbf0ded7913aad9526a032
Michael Luo <michael.luo@berkeley.edu>
2025-01-26 00:29:09 +0000

    Run 32K context length training for 1.5B test run, AMC, AIME and MATH

--- verl/scripts/run_rllm_math_32k.sh
+++ verl/scripts/run_rllm_math_32k.sh
@@ -1,14 +1,14 @@
 set -x
 
 export VLLM_ATTENTION_BACKEND=XFORMERS
-
+# #32768 \
 python3 -m verl.trainer.main_ppo \
     algorithm.adv_estimator=grpo \
     data.train_files=$HOME/data/rllm-math/train.parquet \
     data.val_files=$HOME/data/rllm-math/test.parquet \
     data.train_batch_size=256 \
     data.val_batch_size=500 \
-    data.max_prompt_length=512 \
+    data.max_prompt_length=1024 \
     data.max_response_length=32768 \
     actor_rollout_ref.model.path=deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B \
     actor_rollout_ref.actor.optim.lr=1e-6 \
@@ -24,7 +24,7 @@ python3 -m verl.trainer.main_ppo \
     actor_rollout_ref.actor.fsdp_config.param_offload=False \
     actor_rollout_ref.actor.fsdp_config.grad_offload=False \
     actor_rollout_ref.actor.fsdp_config.optimizer_offload=False \
-    actor_rollout_ref.rollout.tensor_model_parallel_size=1 \
+    actor_rollout_ref.rollout.tensor_model_parallel_size=2 \
     actor_rollout_ref.rollout.name=vllm \
     actor_rollout_ref.rollout.gpu_memory_utilization=0.9 \
     actor_rollout_ref.rollout.n=4 \
@@ -36,7 +36,7 @@ python3 -m verl.trainer.main_ppo \
     trainer.experiment_name='qwen2_7b_function_rm_kl1e-3' \
     +trainer.val_before_train=False \
     trainer.n_gpus_per_node=8 \
-    trainer.nnodes=1 \
-    trainer.save_freq=20 \
+    trainer.nnodes=2 \
+    trainer.save_freq=5 \
     trainer.test_freq=5 \
     trainer.total_epochs=15 $@
