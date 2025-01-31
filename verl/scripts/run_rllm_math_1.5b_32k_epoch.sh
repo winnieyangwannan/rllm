34a513b9016805976c544d975911b1c6cd1b34f5
Michael Luo <michael.luo@berkeley.edu>
2025-01-31 07:51:20 +0000

    Push final run

--- verl/scripts/run_rllm_math_1.5b_32k_epoch.sh
+++ verl/scripts/run_rllm_math_1.5b_32k_epoch.sh
@@ -1,19 +1,20 @@
 set -x
 
 export VLLM_ATTENTION_BACKEND=XFORMERS
-
+# Max context of Qwen is 32k, prompt length is 1024 -> 32k - 1024 = 31744
+# 128, 64 
 python3 -m verl.trainer.main_ppo \
     algorithm.adv_estimator=grpo \
     data.train_files=$HOME/data/deepscaler/train.parquet \
     data.val_files=$HOME/data/deepscaler/test.parquet \
-    data.train_batch_size=128 \
-    data.val_batch_size=512 \
+    data.train_batch_size=64 \
+    data.val_batch_size=30 \
     data.max_prompt_length=1024 \
-    data.max_response_length=32768 \
+    data.max_response_length=31744 \
     actor_rollout_ref.model.path=deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B \
     actor_rollout_ref.actor.optim.lr=1e-6 \
     actor_rollout_ref.model.use_remove_padding=True \
-    actor_rollout_ref.actor.ppo_mini_batch_size=64 \
+    actor_rollout_ref.actor.ppo_mini_batch_size=32 \
     actor_rollout_ref.actor.ppo_epochs=4 \
     actor_rollout_ref.actor.use_dynamic_bsz=True \
     actor_rollout_ref.actor.ppo_max_token_len_per_gpu=24000 \
@@ -30,18 +31,18 @@ python3 -m verl.trainer.main_ppo \
     actor_rollout_ref.rollout.temperature=0.6 \
     actor_rollout_ref.rollout.val_temperature=0.6 \
     actor_rollout_ref.rollout.gpu_memory_utilization=0.85 \
-    actor_rollout_ref.rollout.n=8 \
-    actor_rollout_ref.rollout.n_val=8 \
+    actor_rollout_ref.rollout.n=16 \
+    actor_rollout_ref.rollout.n_val=16 \
     actor_rollout_ref.ref.fsdp_config.param_offload=True \
     algorithm.kl_ctrl.kl_coef=0.001 \
     trainer.critic_warmup=0 \
     trainer.logger=['console','wandb'] \
     trainer.project_name='rllm-o3' \
-    trainer.experiment_name='32k-still-1.5b-epochs' \
+    trainer.experiment_name='1.5b-32k-grpo-epochs' \
     +trainer.val_before_train=True \
     trainer.n_gpus_per_node=8 \
     trainer.nnodes=1 \
-    trainer.save_freq=10 \
-    trainer.test_freq=10 \
+    trainer.save_freq=30 \
+    trainer.test_freq=15 \
     trainer.default_hdfs_dir=null \
     trainer.total_epochs=30 $@
