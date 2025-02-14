d10b071310355a10b4a8c7e0e1b4cf601fa5a5de
Michael Luo <michael.luo@berkeley.edu>
2025-02-14 11:55:04 +0000

    Update

--- scripts/train/debug.sh
+++ scripts/train/debug.sh
@@ -3,7 +3,7 @@ set -x
 
 # Warning: Export VLLM_ATTENTION_BACKEND on every machine before starting Ray cluster.
 # vLLM without XFORMERS will results in CUDA errors.
-export VLLM_ATTENTION_BACKEND=XFORMERS
+export VLLM_ATTENTION_BACKEND=FLASH_ATTN
 
 # Parse command line arguments
 while [[ $# -gt 0 ]]; do
@@ -28,16 +28,16 @@ python3 -m verl.trainer.main_ppo \
     algorithm.adv_estimator=grpo \
     data.train_files=$HOME/rllm/data/train.parquet \
     data.val_files=$HOME/rllm/data/aime.parquet \
-    data.train_batch_size=8 \
+    data.train_batch_size=256 \
     data.val_batch_size=512 \
-    data.max_prompt_length=1024 \
+    data.max_prompt_length=8192 \
     data.max_response_length=1024 \
     actor_rollout_ref.model.path=$MODEL_PATH  \
     actor_rollout_ref.actor.optim.lr=1e-6 \
     actor_rollout_ref.model.use_remove_padding=True \
     actor_rollout_ref.actor.ppo_mini_batch_size=8 \
     actor_rollout_ref.actor.use_dynamic_bsz=True \
-    actor_rollout_ref.actor.ppo_max_token_len_per_gpu=2048 \
+    actor_rollout_ref.actor.ppo_max_token_len_per_gpu=16384 \
     actor_rollout_ref.actor.use_kl_loss=True \
     actor_rollout_ref.actor.kl_loss_coef=0.001 \
     actor_rollout_ref.actor.kl_loss_type=low_var_kl \
@@ -51,18 +51,18 @@ python3 -m verl.trainer.main_ppo \
     actor_rollout_ref.rollout.temperature=0.6 \
     actor_rollout_ref.rollout.val_temperature=0.6 \
     actor_rollout_ref.rollout.gpu_memory_utilization=0.2 \
-    actor_rollout_ref.rollout.n=2 \
-    actor_rollout_ref.rollout.n_val=2 \
+    actor_rollout_ref.rollout.n=8 \
+    actor_rollout_ref.rollout.n_val=8 \
     actor_rollout_ref.ref.fsdp_config.param_offload=True \
     algorithm.kl_ctrl.kl_coef=0.001 \
     trainer.critic_warmup=0 \
     trainer.logger=['console','wandb'] \
     trainer.project_name='deepscaler' \
     trainer.experiment_name='deepscaler-debug' \
-    +trainer.val_before_train=True \
-    trainer.n_gpus_per_node=2 \
+    +trainer.val_before_train=False \
+    trainer.n_gpus_per_node=8 \
     trainer.nnodes=1 \
     trainer.save_freq=1 \
-    trainer.test_freq=1 \
+    trainer.test_freq=10 \
     trainer.default_hdfs_dir=null \
     trainer.total_epochs=30 "${@:1}"
