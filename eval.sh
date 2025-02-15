547f1f725f811d71f481176fadb6347d16ff21c7
lambda7xx <lambda7xx@gmail.com>
2025-02-15 06:14:09 -0800

    add eval script

--- eval.sh
+++ eval.sh
@@ -1,6 +1,5 @@
-
 #DeepSeek-R1-Distill-Qwen-1.5B livecodebench
-./scripts/eval/eval_model.sh --model /data/xiaoxiang/DeepSeek-R1-Distill-Qwen-1.5B  --datasets livecodebench --output-dir /data/xiaoxiang/eval/DeepSeek-R1-Distill-Qwen-1.5B
+./scripts/eval/eval_livecodebench.sh --model /data/xiaoxiang/DeepSeek-R1-Distill-Qwen-1.5B  --datasets livecodebench --output-dir /data/xiaoxiang/eval/DeepSeek-R1-Distill-Qwen-1.5B
 
 #DeepScaleR-1.5B-Preview livecodebench
-./scripts/eval/eval_model.sh --model /data/xiaoxiang/DeepScaleR-1.5B-Preview --datasets livecodebench --output-dir /data/xiaoxiang/eval/DeepScaleR-1.5B-Preview
+./scripts/eval/eval_livecodebench.sh --model /data/xiaoxiang/DeepScaleR-1.5B-Preview --datasets livecodebench --output-dir /data/xiaoxiang/eval/DeepScaleR-1.5B-Preview
