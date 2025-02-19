ca867d51a72355a68da6a80bad432744d9773a9c
Roy Huang <hyfdudu@gmail.com>
2025-02-18 20:56:25 -0800

    change the eval dataset to Qwen/CodeElo

--- rllm/data/preprocess/codeforces.py
+++ rllm/data/preprocess/codeforces.py
@@ -35,16 +35,40 @@ with open("train_codeforces.json", "w") as f:
     json.dump(dataset, f, indent=4)
 
 # %%
-ds = load_dataset("MatrixStudio/Codeforces-Python-Submissions", split="test")
+ds = load_dataset("Qwen/CodeElo", split="test")
 
 print(ds)
+
+# %%
+TEMPLATE = """
+{description}
+
+Input Format:
+{input_format}
+
+Output Format:
+{output_format}
+
+Tags:
+{tags}
+
+Time Limit: {time_limit} ms
+Memory Limit: {memory_limit} MB
+"""
 
 # %%
 dataset = []
 for entry in ds:
     new_entry = {
-        "problem": entry["prompt"] + "\n" + entry["problem-description"],#Note(Xiao):this may have problem
-        "test_cases": entry["test_cases"],#str, when use it as a reward, need use json.loads
+        "problem": TEMPLATE.format(
+            description=entry["description"],
+            input_format=entry["input"],
+            output_format=entry["output"],
+            tags=entry["tags"],
+            time_limit=entry["time_limit_ms"],
+            memory_limit=entry["memory_limit_mb"],
+        ),
+        "test_cases": entry["examples"],#str, when use it as a reward, need use json.loads
     }
     dataset.append(new_entry)
 
