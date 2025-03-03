ba2cc494f52c982e39507772488a75fd609bc78e
lambda7xx <lambda7xx@gmail.com>
2025-03-03 15:12:47 +0000

    clean up

--- tests/rllm/rewards/kill.sh
+++
@@ -1,8 +0,0 @@
-ps -aux | grep "tests_taco.py" | grep -v grep | awk '{print $2}' | xargs kill -9
-
-ps -aux | grep "coder1.py" | grep -v grep | awk '{print $2}' | xargs kill -9
-
-
-# nohup python3 examples/data_preprocess/coder1.py > coder1.log 2>&1 &
-
-# nohup python3 coder1.py > coder1.log 2>&1 &
