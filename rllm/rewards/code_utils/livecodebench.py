0f2cfe6e6ef03165042876a44a99be6878187f28
lambda7xx <lambda7xx@gmail.com>
2025-03-04 01:17:33 +0000

    commit

--- rllm/rewards/code_utils/livecodebench.py
+++ rllm/rewards/code_utils/livecodebench.py
@@ -322,9 +322,10 @@ def run_test(test_cases, completion, timeout, runtime_debug, is_extracted):
     return result
 
 def run_tests_for_one_example(
-    test_cases, completion, result_list, runtime_debug, is_extracted, num_threads=4
+    test_cases, completion, result_list, runtime_debug, is_extracted
 ):
     time_elapsed = float("inf")
+     num_threads=8
     test_type = test_cases[0]["testtype"]
     reliability_guard()
 
