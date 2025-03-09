5bb72cc91111764581ef10a6ce0b9645c3020e9b
Michael Luo <mluo@mk-ix-08.cloud.together.ai>
2025-03-09 01:26:03 -0800

    upload deepscaler 7b

--- rllm/data/dataset_types.py
+++ rllm/data/dataset_types.py
@@ -17,6 +17,7 @@ class TrainDataset:
         GSM8k = 'GSM8K'
         STILL = "STILL"
         DEEPSCALER = "DEEPSCALER"
+        DEEPSCALER_7B = "DEEPSCALER_7B"
     
     class Code(enum.Enum):
         TACO = "TACO"
