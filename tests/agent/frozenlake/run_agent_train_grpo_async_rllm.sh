331e747d24156c5f1d328ea860d1228bdd716e1a
Michael Luo <michael.luo@berkeley.edu>
2025-04-27 11:20:30 +0000

    Working example

--- tests/agent/frozenlake/run_agent_train_grpo_async_rllm.sh
+++ tests/agent/frozenlake/run_agent_train_grpo_async_rllm.sh
@@ -7,71 +7,13 @@ export VLLM_USE_V1=0
 # Find the directory where rllm package is located
 RLLM_DIR=$(python3 -c "import rllm; import os; print(os.path.dirname(os.path.dirname(rllm.__file__)))")
 
-
-# python3 -m rllm.train.train_agent_ppo \
-#     algorithm.adv_estimator=grpo \
-#     data.train_files=${RLLM_DIR}/data/rllm-frozenlake/train.parquet \
-#     data.val_files=${RLLM_DIR}/data/rllm-frozenlake/test.parquet \
-#     data.train_batch_size=8 \
-#     data.val_batch_size=128 \
-#     data.max_prompt_length=1024 \
-#     data.max_response_length=2048 \
-#     actor_rollout_ref.model.path=Qwen/Qwen2.5-7B \
-#     actor_rollout_ref.hybrid_engine=True \
-#     actor_rollout_ref.actor.optim.lr=1e-6 \
-#     actor_rollout_ref.model.use_remove_padding=True \
-#     actor_rollout_ref.actor.ppo_mini_batch_size=8 \
-#     actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=1 \
-#     actor_rollout_ref.actor.use_dynamic_bsz=True \
-#     actor_rollout_ref.actor.ppo_max_token_len_per_gpu=24000 \
-#     actor_rollout_ref.actor.use_kl_loss=False \
-#     actor_rollout_ref.actor.clip_ratio_high=0.28 \
-#     actor_rollout_ref.actor.kl_loss_coef=0.001 \
-#     actor_rollout_ref.actor.kl_loss_type=low_var_kl \
-#     actor_rollout_ref.actor.ulysses_sequence_parallel_size=1 \
-#     actor_rollout_ref.model.enable_gradient_checkpointing=True \
-#     actor_rollout_ref.actor.fsdp_config.param_offload=True \
-#     actor_rollout_ref.actor.fsdp_config.optimizer_offload=True \
-#     actor_rollout_ref.rollout.tensor_model_parallel_size=1 \
-#     actor_rollout_ref.rollout.name=vllm \
-#     actor_rollout_ref.rollout.async_engine=True \
-#     actor_rollout_ref.rollout.enforce_eager=True \
-#     actor_rollout_ref.rollout.enable_log_prob=False \
-#     actor_rollout_ref.rollout.temperature=0.6 \
-#     actor_rollout_ref.rollout.gpu_memory_utilization=0.65 \
-#     actor_rollout_ref.rollout.n=1 \
-#     actor_rollout_ref.rollout.val_kwargs.n=1 \
-#     actor_rollout_ref.rollout.val_kwargs.temperature=0.6 \
-#     actor_rollout_ref.rollout.val_kwargs.top_p=0.95 \
-#     actor_rollout_ref.ref.fsdp_config.param_offload=True \
-#     actor_rollout_ref.ref.log_prob_micro_batch_size_per_gpu=1 \
-#     actor_rollout_ref.rollout.log_prob_micro_batch_size_per_gpu=1 \
-#     actor_rollout_ref.actor.entropy_coeff=0 \
-#     algorithm.kl_ctrl.kl_coef=0.001 \
-#     trainer.critic_warmup=0 \
-#     trainer.logger=['console','wandb'] \
-#     trainer.project_name='deepscaler-agent' \
-#     trainer.experiment_name='7b-grpo-frozenlake_agent' \
-#     trainer.val_before_train=False \
-#     trainer.n_gpus_per_node=8 \
-#     trainer.nnodes=1 \
-#     trainer.save_freq=100 \
-#     trainer.test_freq=10 \
-#     trainer.default_hdfs_dir=null \
-#     env.name=frozenlake \
-#     agent.name=frozenlakeagent \
-#     agent.max_steps=10 \
-#     agent.async_engine=True \
-#     trainer.total_epochs=100
-
-
 python3 -m rllm.train.train_agent_ppo \
     algorithm.adv_estimator=grpo \
     data.train_files=${RLLM_DIR}/data/rllm-frozenlake/train.parquet \
     data.val_files=${RLLM_DIR}/data/rllm-frozenlake/test.parquet \
     data.train_batch_size=64 \
     data.val_batch_size=128 \
-    data.max_prompt_length=1024 \
+    data.max_prompt_length=2048 \
     data.max_response_length=2048 \
     actor_rollout_ref.model.path=Qwen/Qwen2.5-7B \
     actor_rollout_ref.hybrid_engine=True \
@@ -105,6 +47,8 @@ python3 -m rllm.train.train_agent_ppo \
     actor_rollout_ref.rollout.log_prob_micro_batch_size_per_gpu=1 \
     actor_rollout_ref.actor.entropy_coeff=0 \
     algorithm.kl_ctrl.kl_coef=0.001 \
+    algorithm.mask_truncated_samples=False \
+    algorithm.clip_advantages=True \
     trainer.critic_warmup=0 \
     trainer.logger=['console','wandb'] \
     trainer.project_name='deepscaler-agent' \
@@ -113,7 +57,7 @@ python3 -m rllm.train.train_agent_ppo \
     trainer.n_gpus_per_node=8 \
     trainer.n_training_gpus_per_node=4 \
     trainer.nnodes=1 \
-    trainer.save_freq=10 \
+    trainer.save_freq=100 \
     trainer.test_freq=10 \
     trainer.default_hdfs_dir=null \
     env.name=frozenlake \
