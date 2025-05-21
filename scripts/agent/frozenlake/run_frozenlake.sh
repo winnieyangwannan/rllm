d572da5b1bdfe046fe39ae5574260926c7880e15
Michael Luo <michael.luo@berkeley.edu>
2025-05-21 11:03:11 +0000

    Update velr

--- scripts/agent/frozenlake/run_frozenlake.sh
+++ scripts/agent/frozenlake/run_frozenlake.sh
@@ -3,6 +3,8 @@ set -x
 export VLLM_ATTENTION_BACKEND=FLASH_ATTN
 export PYTORCH_CUDA_ALLOC_CONF="expandable_segments:False"
 export VLLM_USE_V1=1
+export VLLM_ALLOW_LONG_MAX_MODEL_LEN=1
+export VLLM_ENGINE_ITERATION_TIMEOUT_S=100000000000
 
 # Find the directory where rllm package is located
 RLLM_DIR=$(python3 -c "import rllm; import os; print(os.path.dirname(os.path.dirname(rllm.__file__)))")
