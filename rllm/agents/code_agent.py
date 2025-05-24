e43dbce9e99f488311eaf543975e3c343e1eae6d
jeffreysijuntan <sijuntan@yeah.net>
2025-05-24 16:18:15 -0700

    fix code agent

--- rllm/agents/code_agent.py
+++ rllm/agents/code_agent.py
@@ -129,7 +129,7 @@ class CompetitionCodingAgent(BaseAgent):
 
         if self.remove_thinking and content.count("</think>") == 1:
             cur_step.thought, cur_step.action = content.split("</think>")
-            cur_step.action += "</think>"        
+            cur_step.thought += "</think>"        
             self.messages.append({"role": "assistant", "content": cur_step.action})
         else:
             cur_step.thought = content 
