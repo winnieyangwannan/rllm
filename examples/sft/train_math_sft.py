f8fef7f82ceac2cf216ebf2de5df7b419ab67942
Manan Roongta <mananr@berkeley.edu>
2025-06-29 16:02:07 -0700

    fixed relative path, init

--- examples/sft/train_math_sft.py
+++ examples/sft/train_math_sft.py
@@ -4,7 +4,7 @@ from omegaconf import DictConfig
 from rllm.trainer.agent_sft_trainer import AgentSFTTrainer
 
 
-@hydra.main(config_path="../../rllm/trainer/config", config_name="sft_trainer", version_base=None)
+@hydra.main(config_path="pkg://rllm.trainer.config", config_name="sft_trainer", version_base=None)
 def main(config: DictConfig):
     # initialize trainer
     trainer = AgentSFTTrainer(config=config)
--- rllm/trainer/config/__init__.py
+++ rllm/trainer/config/__init__.py
@@ -1,3 +1,2 @@
-# Configuration module for rllm trainer
-# This file makes the config directory a Python package
-# and allows Hydra to discover configuration files
+# config for rllm trainer
+
