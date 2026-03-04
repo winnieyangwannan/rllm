90d1c4be386ef787579ad0c8723608f2ecf5aa61
listar2000 <35262801+listar2000@users.noreply.github.com>
2026-03-04 11:23:19 -0600

    hot-fix advantage calcuilation unpacking issue (#416)

--- rllm/experimental/common/rl_algo.py
+++ rllm/experimental/common/rl_algo.py
@@ -3,7 +3,7 @@
 import numpy as np
 
 
-def calculate_grpo_advantages_per_group(rewards: np.ndarray, norm_adv_by_std_in_grpo=True, episilon=1e-6) -> np.ndarray:
+def calculate_grpo_advantages_per_group(rewards: np.ndarray, norm_adv_by_std_in_grpo=True, episilon=1e-6) -> tuple[np.ndarray, np.ndarray]:
     if len(rewards) <= 1:
         group_mean, group_std = 0.0, 1.0
     else:
@@ -15,13 +15,13 @@ def calculate_grpo_advantages_per_group(rewards: np.ndarray, norm_adv_by_std_in_
     else:
         advantages = rewards - group_mean
 
-    return advantages
+    return advantages, advantages
 
 
-def calculate_rloo_advantages_per_group(rewards: np.ndarray) -> np.ndarray:
+def calculate_rloo_advantages_per_group(rewards: np.ndarray) -> tuple[np.ndarray, np.ndarray]:
     num_trajs = len(rewards)
     if num_trajs <= 1:
         return rewards
 
     advantages = num_trajs / (num_trajs - 1) * (rewards - rewards.mean())
-    return advantages
+    return advantages, advantages
